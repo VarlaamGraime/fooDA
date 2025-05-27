b26d7c6f8dd1b010c3697094b3e767d70c17c371
VarlaamGraime <neis01@mail.ru>
2025-05-27 12:58:00 +0300

    fix: добавлен ALLOWED_HOSTS для Render

--- fooDA/settings.py
+++ fooDA/settings.py
@@ -11,21 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-85%-66u=w$qr7d!!)8icv*6h%^%@i7^!r8qrjlju_ez$wh2g)y'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fooda-mvhv.onrender.com']
 
 
 # Application definition
