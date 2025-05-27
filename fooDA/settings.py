626d940b780957199a9adc87fcb1d74909fd6d3a
VarlaamGraime <neis01@mail.ru>
2025-05-27 13:50:51 +0300

    fix

--- fooDA/settings.py
+++ fooDA/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'webapp/static')]
 STATIC_URL = '/static/'
 
 
@@ -46,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fooDA.urls'
